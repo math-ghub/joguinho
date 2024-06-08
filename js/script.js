39c615ba417e6e4790c35180f868a0c9c33c4db9
math-ghub <161179335+math-ghub@users.noreply.github.com>
2024-06-08 02:34:55 -0400

    adiçao de mais javascript

--- js/script.js
+++ js/script.js
@@ -14,8 +14,13 @@ playButton.addEventListener("click", () => {
 const bot = document.querySelector("#bot");
 const rosto = document.querySelector("#rosto");
 const maoDireita = document.querySelector("#mao-direita");
+const maoEsquerda = document.querySelector("#mao-esquerda");
 const msgBox = document.querySelector("#msg-box");
 const msgDelay = 60;
+
+const coposContainer = document.querySelector("#copos-container");
+const copos = document.getElementsByClassName("copo");
+let copoBolinha;
 
 async function startGame() {
     menu.style.display = "none";
@@ -32,7 +37,58 @@ async function startGame() {
     rosto.classList.add("rosto-lingua2");
     maoDireita.classList.add("mao-v")
     const frase1 = msg("tente memorizar onde está a bolinha!", 3);
+    spawnBola();
     await frase1;
+    rosto.classList.remove("rosto-lingua2");
+    maoDireita.classList.remove("mao-v");
+
+    // ---------------------
+    startRound();
+}
+
+function switchCup() {
+
+}
+
+function grab(hand, cup) {
+    const handRect = hand.getBoundingClientRect();
+    const cupRect = cup.getBoundingClientRect();
+
+    const leftVal = hand.style.left != "" ? parseInt(hand.style.left) : 0
+
+    hand.style.left = leftVal + cupRect.left - handRect.left + "px";
+    hand.style.bottom = "-330px";
+}
+
+function selectCup(list) {
+    const rand = Math.round(Math.random() * (list.length - 1));
+    const copo = list[rand];
+    list.splice(rand, 1);
+    return copo;
+}
+
+function spawnBola() {
+    copoBolinha = copos[Math.round(Math.random() * 2)];
+    copoBolinha.classList.add("bolinha");
+}
+
+function startRound() {
+    const coposDisponiveis = Array.from(copos);
+
+    rosto.classList.add("rosto-feliz");
+    maoDireita.classList.add("mao-aberta");
+    msg("", 0);
+
+    maoEsquerda.style.display = "block";
+    maoEsquerda.classList.add("mao-aberta");
+    
+    const copo_esquerdo = selectCup(coposDisponiveis);
+    const copo_direito = selectCup(coposDisponiveis);
+
+    grab(maoEsquerda, copo_esquerdo);
+    grab(maoDireita, copo_direito);
+
+
 }
 
 async function msg(text, delay) {
